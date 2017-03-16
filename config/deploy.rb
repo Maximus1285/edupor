7df74c99969f853c118dd41cd148278f928b0158
Marco Sanabria <msanabria@growthaccelerationpartners.com>
2017-03-15 23:44:05 -0600

    Update repository url in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "3.7.2"
 
 set :application, 'edupor'
-set :repo_url, 'git@github.com:rails-gap/edupor.git'
+set :repo_url, 'git@github.com:Maximus1285/edupor.git'
 set :deploy_to, '/var/www/edupor'
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml', 'config/omniauth.yml')
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
@@ -11,5 +11,5 @@ set :deploy_via, :copy
 set :user, 'deployer'
 set :use_sudo, false
 set :rails_env, 'production'
-set :ssh_options, { forward_agent: true, port: 5422 }
+set :ssh_options, { forward_agent: true, port: 22 }
 set :passenger_restart_with_touch, true
